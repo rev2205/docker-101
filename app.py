f883b7467ecb582138e8a12aa7f8889614ae6ae5
revanth <revanthreddymalapati@gmail>
2026-07-16 22:33:34 +0530

    added multiple routed and logs

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
 @app.route("/")
 def home():
-    return "Hello from Flask running outside docker!!"
+
+    logger.info("Home page accessed")
+    return render_template("index.html")
+
+@app.route("/health")
+def health():
+    logger.info("Health check accessed")
+    return {
+        "status": "healthy"
+    }
+
+@app.route("/about")
+def about():
+    logger.info("about page accessed")
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
